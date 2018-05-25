db7082333cd0ae90b505fb0531024dd85c8d898b
Ayo <31580516+emdele@users.noreply.github.com>
2018-05-25 02:13:15 +0100

    Try this

---
+++ HelloProject.java
@@ -0,0 +1,19 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		for(int x = 1; x<10; x++) {
+		
+		System.out.println("hello world there!! = " + x);
+
+	}
+		
+	System.out.println("This is a git project");
+	
+	System.out.println("This is a git project");
+	System.out.println("This is a git project");
+}
+	
+}
